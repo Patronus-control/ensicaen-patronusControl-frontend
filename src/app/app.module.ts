26bb8e855a4a14db469b900f391104ed6ecb4991
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2023-02-20 12:10:28 +0100

    introducing the action-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActionMenuComponent } from './action-menu/action-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionMenuComponent
   ],
   imports: [
     BrowserModule,
